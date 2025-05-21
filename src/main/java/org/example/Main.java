e7916e2448f6686dea99ce072cdcc8cdd93a7c0e
Aman Sachan <codewithaman78@gmail.com>
2025-05-22 00:45:55 +0530

    ADD: okhttp to make http requests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://example.com/";
+        Request request = new Request.Builder().url(url).build();
+        try(Response response = client.newCall(request).execute()) {
+            if(!response.isSuccessful()) {
+                System.err.println("Unexpected code " + response);
+            }
+            System.out.println(response.body().string());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
